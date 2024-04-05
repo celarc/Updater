d6e9d2a8f59c6124271598d75bd6b9d5a7e172b0
Branko Celarc <admin@bmcvrhnika.onmicrosoft.com>
2024-04-05 16:40:00 +0200

    First push

---
+++ Updater/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Updater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBarControl1 = new DevExpress.XtraEditors.ProgressBarControl();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.progressBarControl2 = new DevExpress.XtraEditors.ProgressBarControl();
+            this.label3 = new System.Windows.Forms.Label();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.progressBarControl1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.progressBarControl2.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton1.ImageOptions.Image")));
+            this.simpleButton1.Location = new System.Drawing.Point(457, 37);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(129, 27);
+            this.simpleButton1.TabIndex = 0;
+            this.simpleButton1.Text = "Preveri posodobitve";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Aplikacija";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(12, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "BMC";
+            // 
+            // progressBarControl1
+            // 
+            this.progressBarControl1.Location = new System.Drawing.Point(106, 37);
+            this.progressBarControl1.Name = "progressBarControl1";
+            this.progressBarControl1.Properties.EndColor = System.Drawing.Color.Lime;
+            this.progressBarControl1.Properties.LookAndFeel.UseDefaultLookAndFeel = false;
+            this.progressBarControl1.Properties.StartColor = System.Drawing.Color.Green;
+            this.progressBarControl1.Size = new System.Drawing.Size(345, 27);
+            this.progressBarControl1.TabIndex = 4;
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton2.ImageOptions.Image")));
+            this.simpleButton2.Location = new System.Drawing.Point(457, 70);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(129, 27);
+            this.simpleButton2.TabIndex = 5;
+            this.simpleButton2.Text = "Preveri posodobitve";
+            this.simpleButton2.Click += new System.EventHandler(this.simpleButton2_Click);
+            // 
+            // progressBarControl2
+            // 
+            this.progressBarControl2.Location = new System.Drawing.Point(106, 70);
+            this.progressBarControl2.Name = "progressBarControl2";
+            this.progressBarControl2.Properties.EndColor = System.Drawing.Color.Lime;
+            this.progressBarControl2.Properties.LookAndFeel.UseDefaultLookAndFeel = false;
+            this.progressBarControl2.Properties.StartColor = System.Drawing.Color.Green;
+            this.progressBarControl2.Size = new System.Drawing.Size(345, 27);
+            this.progressBarControl2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(12, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 18);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Web param";
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.WorkerReportsProgress = true;
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            this.backgroundWorker2.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker2_ProgressChanged);
+            this.backgroundWorker2.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker2_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(598, 338);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBarControl2);
+            this.Controls.Add(this.simpleButton2);
+            this.Controls.Add(this.progressBarControl1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.simpleButton1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Updater";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.progressBarControl1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.progressBarControl2.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private DevExpress.XtraEditors.ProgressBarControl progressBarControl1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+        private DevExpress.XtraEditors.ProgressBarControl progressBarControl2;
+        private System.Windows.Forms.Label label3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+    }
+}
+
---
+++ Updater/Form1.cs
@@ -0,0 +1,388 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using WinSCP;
+using static System.Net.WebRequestMethods;
+
+namespace Updater
+{
+
+    public partial class Form1 : Form
+    {
+        private string potBMC = @"C:\BMC\", potWebParam = @"C:\inetpub\WebParam\";
+        private int percent = 0;
+        private bool autoUpdate = false;
+        private string autoUpdateLog = "", autoUpdateWebLog = "";
+        public Form1()
+        {
+            InitializeComponent();
+            initPath();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ukazCMD = Environment.GetCommandLineArgs();
+            if (ukazCMD.Contains("updateBMC"))
+            {
+                try
+                {
+                    autoUpdateLog += "Začetek avtomatske posodobitve ob " + DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss") + Environment.NewLine;
+                    Process[] processes = Process.GetProcessesByName("BMC");
+                    int i = 0;
+                    foreach (var process in processes)
+                    {
+                        i++;
+                        process.Kill();
+                    }
+                    if (i > 0) autoUpdateLog += "Pred prenosom podatkov sem zaustavil odprete programe, število odprtih programov je bilo: " + i + Environment.NewLine;
+                    autoUpdate = true;
+                    autoUpdateLog += "Začetek prenosa podatkov" + Environment.NewLine;
+                    backgroundWorker1.RunWorkerAsync();
+                }
+                catch { this.Close(); }
+            }
+            if (ukazCMD.Contains("updateWebParam"))
+            {
+                try
+                {
+                    autoUpdateLog += "Začetek avtomatske posodobitve ob " + DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss") + Environment.NewLine;
+                    Process[] processes = Process.GetProcessesByName("WebParam");
+                    int i = 0;
+                    foreach (var process in processes)
+                    {
+                        i++;
+                        process.Kill();
+                    }
+                    if (i > 0) autoUpdateLog += "Pred prenosom podatkov sem zaustavil odprete programe, število odprtih programov je bilo: " + i + Environment.NewLine;
+                    autoUpdate = true;
+                    autoUpdateLog += "Začetek prenosa podatkov" + Environment.NewLine;
+                    backgroundWorker2.RunWorkerAsync();
+                }
+                catch { this.Close(); }
+            }
+        }
+        private void initPath()
+        {
+            try
+            {
+                XmlDataDocument xmldoc = new XmlDataDocument();
+                XmlNodeList xmlnode;
+                FileStream fs = new FileStream(@"BMC.ini", FileMode.Open, FileAccess.Read);
+                xmldoc.Load(fs);
+                fs.Close();
+
+                try
+                {
+                    xmlnode = xmldoc.GetElementsByTagName("POT_BMC");
+                    potBMC = xmlnode[0].InnerText;
+                    if (potBMC.Length == 0) potBMC = "C:\\BMC\\";
+                }
+                catch { }
+                try
+                {
+                    xmlnode = xmldoc.GetElementsByTagName("POT_WEB_PARAM");
+                    potWebParam = xmlnode[0].InnerText;
+                    if (potWebParam.Length == 0) potWebParam = "C:\\inetpub\\WebParam\\";
+                }
+                catch { }
+            }
+            catch { }
+        }
+        private void getFromFTP()
+        {
+            percent = 0;
+            lendth = 0; 
+            i = 1;
+            simpleButton1.Enabled = false;
+            progressBarControl1.Properties.Step = 1;
+            progressBarControl1.Properties.PercentView = true;
+            progressBarControl1.Properties.Maximum = 100;
+            progressBarControl1.Properties.Minimum = 0;
+            progressBarControl1.EditValue = 0;
+            progressBarControl1.Properties.PercentView = true;
+            progressBarControl1.Properties.ShowTitle = true;
+
+            backgroundWorker1.RunWorkerAsync();
+        }
+        private void getFromFTPWebParam()
+        {
+            percent = 0;
+            lendth = 0;
+            i = 1;
+            simpleButton2.Enabled = false;
+            progressBarControl2.Properties.Step = 1;
+            progressBarControl2.Properties.PercentView = true;
+            progressBarControl2.Properties.Maximum = 100;
+            progressBarControl2.Properties.Minimum = 0;
+            progressBarControl2.EditValue = 0;
+            progressBarControl2.Properties.PercentView = true;
+            progressBarControl2.Properties.ShowTitle = true;
+
+            backgroundWorker2.RunWorkerAsync();
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            if (System.Diagnostics.Process.GetProcessesByName("BMC").Count() > 0) MessageBox.Show("Zaprite vse oprte BMC programe!");
+            else
+            {
+                string message = "Ali preverim in prenesem posodobitve za program BMC?";
+                string caption = "Potrdi";
+                MessageBoxButtons buttons = MessageBoxButtons.OKCancel;
+                DialogResult result;
+
+                result = MessageBox.Show(message, caption, buttons);
+
+                if (result == System.Windows.Forms.DialogResult.OK)
+                {
+                    getFromFTP();
+                }
+
+            }
+        }
+        private void simpleButton2_Click(object sender, EventArgs e)
+        {
+            if (System.Diagnostics.Process.GetProcessesByName("WebParam").Count() > 0) MessageBox.Show("Zaprite vse oprte Web Param programe!");
+            else
+            {
+                string message = "Ali preverim in prenesem posodobitve za program WebParam?";
+                string caption = "Potrdi";
+                MessageBoxButtons buttons = MessageBoxButtons.OKCancel;
+                DialogResult result;
+
+                result = MessageBox.Show(message, caption, buttons);
+
+                if (result == System.Windows.Forms.DialogResult.OK)
+                {
+                    getFromFTPWebParam();
+                }
+
+            }
+        }
+        private double lendth = 0, i = 1;
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var backgroundWorker = sender as BackgroundWorker;
+            try
+            {
+
+                string url = "bmc.si", username = "updater@bmc.si", password = "fcc1b727289ac03db7e76f6291039923";
+
+                SessionOptions sessionOptions = new SessionOptions
+                {
+                    Protocol = Protocol.Ftp,
+                    HostName = url,
+                    UserName = username,
+                    Password = password,
+                };
+
+                using (Session session = new Session())
+                {
+                    string localPath = potBMC;
+                    // Connect
+                    session.Open(sessionOptions);
+
+                    string remotePath = @"/1/";
+                    RemoteDirectoryInfo directoryInfo = session.ListDirectory(remotePath);
+                    lendth = directoryInfo.Files.Count;
+
+                    downloadFiles(directoryInfo,session,localPath,remotePath,backgroundWorker);
+
+                    // Select the most recent file
+
+
+                    //RemoteFileInfo latest = directoryInfo.Files.OrderByDescending(file => file.LastWriteTime).First();
+
+                    // Download the selected file
+
+
+                }
+            }
+            catch (Exception ex) { backgroundWorker.ReportProgress(-1, ex); }
+        }
+
+        private bool rootLevel = true;
+        private void downloadFiles(RemoteDirectoryInfo directoryInfo, Session session, string localPath, string remotePath, BackgroundWorker backgroundWorker)
+        {
+            
+            foreach (RemoteFileInfo rfi in directoryInfo.Files)
+            {
+                if (rfi.IsDirectory && rfi.Name.Replace(".", "").Length > 0)
+                {
+                    rootLevel = false;
+                    downloadFiles(session.ListDirectory(remotePath + rfi.Name + "/"), session,localPath + rfi.Name + "\\", remotePath + rfi.Name + "/", backgroundWorker);
+                    rootLevel = true;
+                }
+                else
+                {//MessageBox.Show(rfi.Name + " " + rfi.LastWriteTime + " Time local: " + System.IO.File.GetLastWriteTime(localPath + rfi.Name) + "     Type:" + rfi.FileType);
+                    if (rfi.LastWriteTime != System.IO.File.GetLastWriteTime(localPath + rfi.Name) && rfi.Name.Replace(".", "").Length > 0 && rfi.Name.Split('.').Last() != "fdb" && rfi.Name.Split('.').Last() != "FDB")
+                    {
+                        string sourcePath = RemotePath.EscapeFileMask(remotePath + rfi.Name);
+                        TransferOperationResult transferResult = session.GetFiles(sourcePath, localPath);
+                        transferResult.Check();
+
+                        foreach (TransferEventArgs transfer in transferResult.Transfers)
+                        {
+                            autoUpdateLog += "Prenos datoteke " + transfer.FileName + " uspešen." + Environment.NewLine;
+                        }
+                    }
+
+                    if (rootLevel)
+                    {
+                        i++;
+                        backgroundWorker.ReportProgress(Convert.ToInt32((i / lendth) * 100));
+                    }
+                }
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+
+            if (e.ProgressPercentage == -1)
+            {
+                Exception ex = (Exception)e.UserState;
+                MessageBox.Show(ex.Message + Environment.NewLine + Environment.NewLine + ex.InnerException + Environment.NewLine + Environment.NewLine + ex.Source + Environment.NewLine + Environment.NewLine + ex.StackTrace + Environment.NewLine + Environment.NewLine + ex.TargetSite);
+            }
+            else
+            {
+                progressBarControl1.EditValue = e.ProgressPercentage;
+                //progressBarControl1.PerformStep();
+                percent = e.ProgressPercentage;
+                progressBarControl1.Update();
+            }
+
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            BackgroundWorker b1 = (BackgroundWorker)sender;
+            if (autoUpdate)
+            {
+                if (percent != 100) autoUpdateLog += "Med prenosom je prišlo do napake.";
+                else autoUpdateLog += "Posodobitve uspešno prenešene, konec prenosa podatkov ob: " + DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss") + Environment.NewLine + Environment.NewLine;
+
+                try
+                {
+                    if (!Directory.Exists(potBMC + @"Log\")) Directory.CreateDirectory(potBMC + @"Log\");
+                    System.IO.File.AppendAllText(potBMC + @"Log\AutoUpdate.txt", autoUpdateLog);
+                }
+                catch { }
+                this.Close();
+            }
+            else
+            {
+                if (percent != 100) MessageBox.Show("Med prenosom je prišlo do napake. Preverite internetno povezavo in preverite če je kje še kje odprt BMC program.");
+                else MessageBox.Show("Prenos končan!");
+                simpleButton1.Enabled = true;
+            }
+        }
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var backgroundWorker = sender as BackgroundWorker;
+            try
+            {
+
+                string url = "bmc.si", username = "updater@bmc.si", password = "fcc1b727289ac03db7e76f6291039923";
+
+                SessionOptions sessionOptions = new SessionOptions
+                {
+                    Protocol = Protocol.Ftp,
+                    HostName = url,
+                    UserName = username,
+                    Password = password,
+                };
+
+                using (Session session = new Session())
+                {
+                    string localPath = potWebParam;
+                    // Connect
+                    session.Open(sessionOptions);
+
+                    string remotePath = @"/6/";
+                    RemoteDirectoryInfo directoryInfo = session.ListDirectory(remotePath);
+
+                    double lendth = directoryInfo.Files.Count, i = 0;
+
+
+                    // Select the most recent file
+                    foreach (RemoteFileInfo rfi in directoryInfo.Files)
+                    {
+                        //MessageBox.Show(rfi.Name + " " + rfi.LastWriteTime + " Time local: " + System.IO.File.GetLastWriteTime(localPath + rfi.Name) + "     Type:" + rfi.FileType);
+                        if (rfi.LastWriteTime != System.IO.File.GetLastWriteTime(localPath + rfi.Name) && rfi.Name.Replace(".", "").Length > 0 && rfi.Name.Split('.').Last() != "fdb" && rfi.Name.Split('.').Last() != "FDB")
+                        {
+                            string sourcePath = RemotePath.EscapeFileMask(remotePath + rfi.Name);
+                            TransferOperationResult transferResult = session.GetFiles(sourcePath, localPath);
+                            transferResult.Check();
+
+                            foreach (TransferEventArgs transfer in transferResult.Transfers)
+                            {
+                                autoUpdateWebLog += "Prenos datoteke " + transfer.FileName + " uspešen." + Environment.NewLine;
+                            }
+                        }
+                        i++;
+                        backgroundWorker.ReportProgress(Convert.ToInt32((i / lendth) * 100));
+                    }
+
+                    //RemoteFileInfo latest = directoryInfo.Files.OrderByDescending(file => file.LastWriteTime).First();
+
+                    // Download the selected file
+
+
+                }
+            }
+            catch (Exception ex) { backgroundWorker.ReportProgress(-1, ex); }
+        }
+
+        private void backgroundWorker2_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if (e.ProgressPercentage == -1)
+            {
+                Exception ex = (Exception)e.UserState;
+                MessageBox.Show(ex.Message);
+            }
+            else
+            {
+                progressBarControl2.EditValue = e.ProgressPercentage;
+                //progressBarControl1.PerformStep();
+                percent = e.ProgressPercentage;
+                progressBarControl2.Update();
+            }
+        }
+
+        private void backgroundWorker2_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (autoUpdate)
+            {
+                if (percent != 100) autoUpdateWebLog += "Med prenosom je prišlo do napake.";
+                else autoUpdateWebLog += "Posodobitve uspešno prenešene, konec prenosa podatkov ob: " + DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss") + Environment.NewLine + Environment.NewLine;
+
+                try
+                {
+                    if (!Directory.Exists(potWebParam + @"Log\")) Directory.CreateDirectory(potWebParam + @"Log\");
+                    System.IO.File.AppendAllText(potWebParam + @"Log\AutoUpdate.txt", autoUpdateWebLog);
+                }
+                catch { }
+                this.Close();
+            }
+            else
+            {
+                if (percent != 100) MessageBox.Show("Med prenosom je prišlo do napake. Preverite internetno povezavo in preverite če je kje še kje odprt BMC program.");
+                else MessageBox.Show("Prenos končan!");
+                simpleButton1.Enabled = true;
+            }
+        }
+    }
+}
+
